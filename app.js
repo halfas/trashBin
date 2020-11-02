69457ca61de180b428919b9578cde542ab696636
ripo4ek <Lyap-andr@yandex.ru>
2020-11-02 23:32:46 +0300

    Update app.js

--- app.js
+++ app.js
@@ -10,9 +10,11 @@ app.get('/login/', (req, res) => {
   res.send('rip123123')
 });
 app.get('/promise/', (req, res) => {
+  res.set('Access-Control-Allow-Origin', '*');
   res.send('function task(x){ return new Promise((resolve,rejected) =>{x > 18 ? resolve("yes"): rejected("no")})}')
 });
 app.get('/fetch/', (req, res) => {
+  res.set('Access-Control-Allow-Origin', '*');
   res.sendFile(__dirname + '/index.html')
 });
 var port = process.env.PORT || 5000;
