b76751545d7f842166ba7044fadbeb094d991d93
Andrey Lyapustin <lyap-andr@yandex.ru>
2020-11-02 23:03:05 +0300

    init

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello Express app!')
+});
+app.get('/login/', (req, res) => {
+  res.set('Access-Control-Allow-Origin', '*');
+  res.send('rip123123')
+});
+app.get('/promise/', (req, res) => {
+  res.send('function task(x){ return new Promise((resolve,rejected) =>{x < 18 ? resolve("yes"): rejected("no")})}')
+});
+app.get('/fetch/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+});
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
