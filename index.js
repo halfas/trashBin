679eb91fc02db9879f847455758e932305ba664b
ripo4ek <Lyap-andr@yandex.ru>
2020-11-28 19:10:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import bodyParser from 'body-parser';
 import {createReadStream} from 'fs';
 import http from 'http';
 import crypto from 'crypto';
-import {appSrc} from './app.js';
+import appSrc from './app.js';
 
 const app = appSrc(express, bodyParser, createReadStream, crypto, http);
 
