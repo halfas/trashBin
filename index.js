8643219d88bf8984d38f370a34339378b15ee754
ripo4ek <Lyap-andr@yandex.ru>
2020-12-14 20:54:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ import http from 'http';
 import crypto from 'crypto';
 import appSrc from './app.js';
 import mongoose  from 'mongoose';
+import fetch from 'node-fetch';
 
-const app = appSrc(express, bodyParser, createReadStream, crypto, http, mongoose);
+const app = appSrc(express, bodyParser, createReadStream, crypto, http, mongoose,fetch);
 
 app.listen(process.env.PORT || 5000);
